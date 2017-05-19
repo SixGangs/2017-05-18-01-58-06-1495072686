0782f81741b3f8c6645333c447db9c20e8014225
SixGangs <2577944135@qq.com>
2017-05-19 15:22:03 +0800

    一次性提交所有文件

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
-module.exports = function main() {
+module.exports = function main(s) {
   // Write your cade here
+  var sum = 0;
+  for(i = 0; i < s.length;i++)
+  {
+  	ss = s.charAt(i);
+  	k = parseInt(ss);
+  	sum = sum + k;
+  }
+  return sum;
 };
